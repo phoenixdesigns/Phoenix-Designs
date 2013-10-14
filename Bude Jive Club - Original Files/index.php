c14fcf79140f4a32c6b48cecd4d8504aba8919c5
phoenixdesigns <jrdjarrold@JJ-MBP.local>
2013-10-14 14:32:11 +0100

    Original Files
    Bude Jive Club original webiste files.

---
+++ Bude Jive Club - Original Files/index.php
@@ -0,0 +1,57 @@
+<?php
+include 'header.php';
+?>
+	<div id="middle-left">
+		<p>Club resumes Mon Sep 30<sup>th</sup> - 10 weeks for &pound;32 or 6 weeks for &pound;22</p>
+		<!-- <h2><a href="./images/summer_dances.pdf" target="_blank">'Summer Dances at Bude Jive Club'</a></h2> -->
+		<h3>Bude Jive Club</h3>
+		<p>Bude Jive Club has been in existence for several years and is continuing to grow in popularity; we meet at The Parkhouse Centre, Bude on Monday nights. The interest in our club arises from our combining two dance styles, traditional rock and roll and jive.</p>
+		<h3>What do we offer?</h3>
+		<p>The Club regularly offers courses of 'beginners and improvers' lessons.  On a regular basis guest instructors give lessons for the 'improvers'.  Rock and rollers who learned to dance in their youth are very welcome at any of the events held by the club.</p>
+		<h3>Our Club</h3>
+		<p>Bude Jive Club has fast gained a reputation as a friendly club and has a very varied social diary supported by Rock and Roll club members throughout the southwest.</p>
+		<!-- Throughout the year dances are run with live bands, record hops and other events. The live band nights usually take place at the Bullers Arms, Marhamchurch, near Bude.</p> -->
+		<h3>The Picture Gallery</h3>
+		<p>Photographs of club nights, special events and the Bude carnival can all be viewed at:
+		<a href="http://www.picasaweb.google.com/budejiveclub" target="_blank">The on-line gallery</a>
+		</p>
+	</div>
+	<div id="middle-right">
+		<h3>Upcoming Events</h3>
+		<table><tr><th>Date</th><th>Band</th></tr>
+		  <tr><td>Sat 09/02/13@8pm</td><td>Valentines - The Kingcats + DJ</td></tr>
+		  <tr><td>Sat 13/04/13@7:30pm</td><td>Play It Cool &amp; Bernie Woods + DJ</td></tr>
+		  <tr><td>Sat 15/06/13@7:30pm</td><td>This Little Girl &amp;  The Dillicats + DJ</td></tr>
+		  <tr><td>Sat 17/08/13@7:30pm</td><td>Carnival Dance (59 Ford) + DJ</td></tr>
+		  <tr><td>Sat 19/10/13@7:30pm</td><td>The Alligators + DJ</td></tr>
+		  <tr><td>Sat 14/12/13@7:30pm</td><td>Xmas Dance - The Bobcats + DJ</td></tr>
+		  <tr><td>Sat 08/02/14@8pm</td><td>Valentines - The Kingcats + DJ</td></tr>
+		  <tr><td>Sat 12/04/14@8pm</td><td>The Unconditionals + DJ</td></tr>
+ 		  <tr><td>Sat 07/06/14@8pm</td><td>ALL DAYER - Pete Jackson &amp; The Zodiacs + Mark Keeleys GRT &amp; Fakin Stevens + DJ</td></tr>
+ 		  <tr><td>Sat 16/07/14@7:30pm</td><td>Carnival Dance = Rockin The Joint + DJ</td></tr>
+		  <tr><td>Sat 13/12/14@7:30pm</td><td>Xmas Dance - 59 Ford + DJ</td></tr>
+		</table>
+		<h3>Useful Links</h3>
+		<h4>Clothing</h4>
+		<a href="http://www.wagtailsdancewear.co.uk" target="_blank">Wagtails Dance Wear</a><br />
+		<a href="http://www.dance-krazy.com/" target="_blank">Dance Krazy</a><br />
+		<a href="http://www.marchharevintage.co.uk/" target="_blank">The March Hare: 50's clothes made to measure</a><br />
+		<a href="http://www.vivienofholloway.com/" target="_blank">Vivien of Holloway</a><br />		
+		<h4>Useful Sites</h4>
+		<a href="http://www.paulnmariesrocknrollevents.com" target="_blank">Paul 'n' Maries R 'n' R Events (Note New URL!)</a><br />
+		<a href="http://www.jiveoholic.org.uk/default.asp" target="_blank">Jiveoholics</a><br />
+		<a href="http://www.lemonrock.com/" target="_blank">Lemon Rock Gig Guide</a><br />
+		<a href="http://www.bluesuedenews.co.uk/" target="_blank">Maggies Blue Suede news</a><br /><br />
+
+	</div>
+	<div id="gallery">
+		<p>Gallery - [
+		<a href="http://www.picasaweb.google.com/budejiveclub" target="_blank">On-line gallery</a>]</p>
+		<img src="images/d1.jpg" alt="The Guys"/>
+		<img src="images/d2.jpg" alt="Cheers"/>
+		<img src="images/d3.jpg" alt="Ready to rock"/>
+		<img src="images/d4.jpg" alt="Double Jive"/>
+	</div>
+<?php
+include 'footer.php';
+?>
---
+++ Bude Jive Club - Original Files/links.php
@@ -0,0 +1,33 @@
+<?php
+include 'header.php';
+?>
+	<div id="middle-left">
+		<h2>Useful Links</h2>
+		<h3>Clothing</h3>
+		<a href="http://www.wagtailsdancewear.co.uk" target="_blank">Wagtails Dance Wear</a><br />
+		<a href="http://www.dance-krazy.com/" target="_blank">Dance Krazy</a><br />
+		<a href="http://www.marchharevintage.co.uk/" target="_blank">The March Hare: 50's clothes made to measure</a><br />
+		<a href="http://www.vivienofholloway.com/" target="_blank">Vivien of Holloway</a><br />
+		<h3>Other Clubs</h3>
+		<a href="http://www.paulnmariesrocknrollevents.com" target="_blank">Paul 'n' Maries R 'n' R Events (note new URL!)</a><br />
+		<a href="http://www.westernunionjiveclub.co.uk/index.html" target="_blank">Western Union Jive Club</a><br />
+		<a href="http://www.southwestswing.org.uk/" target="_blank">SouthWest Swing</a><br />
+		<h3>Useful Sites</h3>
+		<a href="http://www.myholidaycottagecornwall.co.uk" target="_blank">Penfound Lodge Holiday Cottage</a><br />
+		<a href="http://www.hodgsoninsurance.co.uk" target="_blank">GJT Hodgson Insurance Services</a><br />
+		<a href="http://www.guesthouseinsurance.co.uk" target="_blank">Guest House Insurance</a><br />
+		<a href="http://www.budetownfc.co.uk" target="_blank">Bude Town Football Club</a><br />
+		<a href="http://www.jiveoholic.org.uk/default.asp" target="_blank">Jiveoholics</a><br />
+		<a href="http://http://www.bluesuedenews.co.uk/" target="_blank">Maggies Blue Suede news</a><br />
+		<a href="http://www.wildbobburgos.co.uk/" target="_blank">Wild Bob Burgos</a><br />
+
+	</div>
+	<div id="middle-right">
+		<h2>Our Venue: Parkhouse Centre, Bude</h2>
+		<iframe width="300" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.co.uk/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=parkhouse,+bude&amp;sll=53.800651,-4.064941&amp;sspn=12.344606,37.529297&amp;ie=UTF8&amp;s=AARTsJpiKk8manbChxbclNyQ6YWnJrXU2w&amp;ll=50.829036,-4.546452&amp;spn=0.016264,0.025749&amp;z=14&amp;output=embed"></iframe><br /><small><a href="http://maps.google.co.uk/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=parkhouse,+bude&amp;sll=53.800651,-4.064941&amp;sspn=12.344606,37.529297&amp;ie=UTF8&amp;ll=50.829036,-4.546452&amp;spn=0.016264,0.025749&amp;z=14" style="color:#0000FF;text-align:left">View Larger Map</a></small>
+		<p>Parkhouse Centre, The Crescent, Bude, Cornwall EX23 8LD</p>
+	</div>
+	<div id="gallery"></div>
+<?php
+include 'footer.php';
+?>
